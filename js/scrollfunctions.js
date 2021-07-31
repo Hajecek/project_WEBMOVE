16e49ad599c545e566fd9a58fa7abac08b416fad
Hajecek <michal.hajek8@seznam.cz>
2021-07-31 20:59:46 +0200

    edit

--- js/scrollfunctions.js
+++ js/scrollfunctions.js
@@ -11,11 +11,15 @@ $(function () {
   $(window).scroll(function () {
     var height = $('body').height();
     var scrollTop = $(window).scrollTop();
-    if(scrollTop == 0){
-         $('.logo_small_left').css({ "display" : "block",});
+    if(scrollTop <= 0){
+         $('.logo_small_left').css({ "display" : "inline",});
          $('.navbar-fixed-top').css({ "position" : "fixed", "top" : "0px"});
+         setTimeout(function(){  $('#navbar').css({ "background" : "transparent",}); }, 1000);
     }
     else {
-         $('.logo_small_left').css({ "display" : "none",});
+         $('.logo_small_left').css({ "display" : "flex",});
+         setTimeout(function(){  $('#navbar').css({ "background" : "rgba(28, 35, 46, 0.363)",}); }, 100);
+         
+        
    }
 });
