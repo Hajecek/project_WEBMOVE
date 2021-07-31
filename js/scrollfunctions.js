927f47a027ffb1a49efba780a5f0b3228ad4d246
Hajecek <michal.hajek8@seznam.cz>
2021-07-31 15:51:34 +0200

    edit

--- js/scrollfunctions.js
+++ js/scrollfunctions.js
@@ -2,5 +2,19 @@ $(function () {
     $(document).scroll(function () {
         var $nav = $(".navbar-fixed-top");
         $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
+
       });
-  });
+  }); 
+
+
+
+  $(window).scroll(function () {
+    var height = $('body').height();
+    var scrollTop = $(window).scrollTop();
+    if(scrollTop == 0){
+         $('.logo_small_left').css({ "display" : "block",});
+    }
+    else {
+         $('.logo_small_left').css({ "display" : "none",});
+   }
+});
