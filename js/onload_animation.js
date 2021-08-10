f4a78611441f13f50e77c71747f655acafe9e710
Hajecek <michal.hajek8@seznam.cz>
2021-08-10 20:31:47 +0200

    edit

---
+++ js/onload_animation.js
@@ -0,0 +1,7 @@
+$(document).ready(function () {
+
+document.querySelector("span.web").style.width="100%";
+setTimeout(function(){ document.querySelector("span.web").style.width="0%"; }, 2000);
+
+
+})
