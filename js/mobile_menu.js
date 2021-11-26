a2959a3716f0dcab7f563a36c71f8bf18aa8f948
Hajecek <michal.hajek8@seznam.cz>
2021-11-26 21:22:27 +0100

    edit

--- js/mobile_menu.js
+++ js/mobile_menu.js
@@ -1,10 +1,11 @@
 let menutoggle = document.querySelector('.toggle');
 let bb = document.querySelector('.menu_mobile_onclick');
-let html = document.querySelector('html');
+let aa = document.querySelector("html");
+
 menutoggle.onclick= function(){
     menutoggle.classList.toggle('active');
     bb.classList.toggle('active');
-    html.classList.toggle('active');
+    aa.classList.toggle('active');
 
    
 } 
