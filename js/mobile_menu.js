5f56ab18afd7f6b46d5db00198f7bb32d2a57d72
Michal Hájek <webmovecz@gmail.com>
2021-12-10 12:19:29 +0100

    edit

--- js/mobile_menu.js
+++ js/mobile_menu.js
@@ -1,18 +1,21 @@
 let menutoggle = document.querySelector('.toggle');
 let bb = document.querySelector('.menu_mobile_onclick');
 let aa = document.querySelector("html");
-
+let social_media = document.querySelector(".social_media");
 let three_span = document.querySelector(".three_span");
-
+let arrow_footer = document.querySelector(".arrow_footer");
 
 menutoggle.onclick= function(){
     menutoggle.classList.toggle('active');
     bb.classList.toggle('active');
     aa.classList.toggle('active');
     three_span.classList.toggle('active');
-   
+    social_media.classList.toggle('active');
+    arrow_footer.classList.toggle('active');
 } ;
 
 
+
+   
 
 
