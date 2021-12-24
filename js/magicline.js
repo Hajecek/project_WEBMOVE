ed7cd433dd1cf80eef5b4fd0547c877f235e86fe
Hajecek <michal.hajek8@seznam.cz>
2021-12-24 14:58:21 +0100

    edit

--- js/magicline.js
+++ js/magicline.js
@@ -77,36 +77,3 @@ $slidingMenu.find('li a').hover(function () {
 
 
 
-/*SHOW SECTION ONAS*/
-function ahojky(){
-  $(".group li:nth-child(1)").removeClass("active");
-  $(".onas").addClass("animate__animated animate__fadeIn");
-  $(".hh").addClass("animate__animated animate__fadeInLeft animate__delay-1s");
-  $(".name").addClass("animate__animated animate__fadeInDown animate__delay-0.5s");
- /*WEBMOVE 1.0 anima.*/ $(".webmove_number").addClass("animate__animated animate__fadeInDown animate__delay-1s"); 
-document.querySelector(".home").style.display="none";
-$(".group li:nth-child(2)").addClass("active");
-document.querySelector(".onas").style.display="block";
-
-
-$(".li_one").removeClass("active");
-$(".li_two").addClass("active");
-
-}
-
-
-
-/*SHOW SECTION HOME*/
-function domu(){
-  $(".group li:nth-child(2)").removeClass("active");
-  document.querySelector(".onas").style.display="none";
-  $(".group li:nth-child(1)").addClass("active");
-  $(".home").addClass("animate__animated animate__fadeIn");
-  document.querySelector(".home").style.display="block";
- 
-  
-
-  $(".li_one").addClass("active");
-  $(".li_two").removeClass("active");
-
-}
---
+++ js/switch_section.js
@@ -0,0 +1,42 @@
+/*SHOW SECTION ONAS*/
+function ahojky(){
+    $(".group li:nth-child(1)").removeClass("active");
+    $(".onas").addClass("animate__animated animate__fadeIn");
+    $(".hh").addClass("animate__animated animate__fadeInLeft animate__delay-1s");
+    $(".name").addClass("animate__animated animate__fadeInDown animate__delay-0.5s");
+   /*WEBMOVE 1.0 anima.*/ $(".webmove_number").addClass("animate__animated animate__fadeInDown animate__delay-1s"); 
+  document.querySelector(".home").style.display="none";
+  $(".group li:nth-child(2)").addClass("active");
+  document.querySelector(".preload_section").style.display="flex";
+  
+  const timeout_loading = setTimeout(myGreeting, 1500);
+function myGreeting() {
+    document.querySelector(".onas").style.display="block";
+    document.querySelector(".preload_section").style.display="none";
+    return timeout_loading();
+}
+
+
+  
+  
+  $(".li_one").removeClass("active");
+  $(".li_two").addClass("active");
+  
+  }
+  
+  
+  
+  /*SHOW SECTION HOME*/
+  function domu(){
+    $(".group li:nth-child(2)").removeClass("active");
+    document.querySelector(".onas").style.display="none";
+    $(".group li:nth-child(1)").addClass("active");
+    $(".home").addClass("animate__animated animate__fadeIn");
+    document.querySelector(".home").style.display="block";
+   
+    
+  
+    $(".li_one").addClass("active");
+    $(".li_two").removeClass("active");
+  
+  }
